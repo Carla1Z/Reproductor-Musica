cc518e56bc66e2f962bae0659162f5b69a839719
Carla1Z <carla_sabrina_zarate@hotmail.com>
2024-02-25 19:03:38 -0300

    view model

---
+++ ReproductorMusica/VistaModelo/VMlistamusica.cs
@@ -0,0 +1,48 @@
+﻿using ReproductorMusica.VistaModelo;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using ReproductorMusica.Datos;
+using ReproductorMusica.Modelo;
+
+namespace ReproductorMusica.VistaModelo
+{
+    public class VMlistamusica:BaseViewModel
+    {
+        #region VARIABLES
+        List<Mmusica> _listamusica;
+        #endregion
+        #region CONSTRUCTOR
+        public VMlistamusica(INavigation navigation)
+        {
+            Navigation = navigation;
+            MostrarMusica();
+        }
+        #endregion
+        #region OBJETOS
+        public List<Mmusica> ListaMusica
+        {
+            get { return _listamusica; }
+            set { SetValue(ref _listamusica, value); }
+        }
+        #endregion
+        #region PROCESOS
+        public async Task MostrarMusica()
+        {
+            var funcion = new Dmusica();
+            ListaMusica = await funcion.ListarMusica();
+        }
+        public void ProcesoSimple()
+        {
+
+        }
+        #endregion
+        #region COMANDOS
+        //public ICommand ProcesoAsyncommand => new Command(async () => await ProcesoAsyncrono());
+        public ICommand ProcesoSimpcommand => new Command(ProcesoSimple);
+        #endregion
+    }
+}
